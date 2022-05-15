cf035bcb0c975579021956e12726d6020a646bf3
Anurag <kr.anurag24@gmail.com>
2022-05-15 06:52:54 +0000

    add seo stuffn analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,51 @@ import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
 import { Toaster } from 'react-hot-toast'
 import '@fontsource/fira-code'
+import "@fontsource/inter"
+import { NextSeo } from 'next-seo'
+import Head from "next/head"
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700;900&display=swap"
-          rel="stylesheet"
-        />
-      </head>
+      {process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL &&
+        process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID && (
+          <Script
+            src={process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL}
+            data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+            strategy="lazyOnload"
+          />
+        )}
+      <NextSeo
+        title="NFTank"
+        titleTemplate="NFTank"
+        defaultTitle="NFTank"
+        description="NFTank (NFT tank for dummies) will allow developers to quickly request NFTs to personal wallets or smart contracts in just a few clicks."
+        canonical="https://nftank.xyz"
+        openGraph={{
+          url: 'https://nftank.xyz',
+          title: 'NFTanj',
+          description:
+            'NFTank (NFT tank for dummies) will allow developers to quickly request NFTs to personal wallets or smart contracts in just a few clicks.',
+          images: [
+            {
+              url: `/og-image.png`,
+              width: 800,
+              height: 420,
+              alt: 'og image',
+            },
+          ],
+        }}
+        twitter={{
+          handle: '@buidlershub',
+          site: '@buidlershub',
+          cardType: 'summary_large_image',
+        }}
+      />
+      <Head>
+        <link rel="icon" href="/assets/logo.png" type="image/png" />
+      </Head>
       <RecoilRoot>
         <Component {...pageProps} />
         <Toaster />
